0be8a5a2b05c27c767e82d5a48a8f8c7fc1e0644
maneeshabitla <maneesha.s@bitlasoft.com>
2017-04-04 13:07:11 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Git::Application.config.secret_token = 'd977f9529a576122c7aa5adb521e222700faf3f3e2f0fdf1b6b71df10d59140173ef05eb00b6803ddda8048dada697229cca4278faba83b70d7f55cd0ae2ce55'
